858ba2d73486991f5c040657827c8025fe1a923a
Enzo Mescall <emm121@duke.edu>
2023-11-11 00:33:31 -0500

    made plots and stuff with pharmacy deserts

--- analysis.py
+++ analysis.py
@@ -21,13 +21,31 @@ gdf_counties = gpd.read_file(nc_counties_url)
 # Filter North Carolina counties
 nc_counties = gdf_counties[gdf_counties['STUSPS'] == 'NC']
 
-# Plot North Carolina counties with the pharmacies
+# Import spatial.csv from NC_Census_2021
+census_df = pd.read_csv('NC_Census_2021/spatial.csv')
+
+
+# Create a GeoDataFrame from the spatial data's Latitude and Longitude columns
+census_gdf = gpd.GeoDataFrame(
+    census_df, 
+    geometry=gpd.points_from_xy(census_df['Longitude'], census_df['Latitude']),
+    crs='EPSG:4326'
+)
+
+# Use 'GeoSeries.to_crs()' to re-project all measures
+census_gdf = census_gdf.to_crs('EPSG:32119')
+pharmacies_gdf = pharmacies_gdf.to_crs('EPSG:32119')
+nc_counties = nc_counties.to_crs('EPSG:32119')
+
+# Calculate distance to nearest pharmacy for each census district
+census_gdf['distance_to_pharmacy'] = census_gdf.geometry.apply(lambda x: pharmacies_gdf.distance(x).min())
+
+# Add a binary variable that measures whether the distance is greater or lesser than one mile
+census_gdf['within_one_mile'] = census_gdf['distance_to_pharmacy'] <= 1609.34  # 1 mile = 1609.34 meters
+
+# Plot the census data with the new column
 fig, ax = plt.subplots(figsize=(12, 12))
 nc_counties.plot(ax=ax, color='white', edgecolor='black')
-pharmacies_gdf.plot(ax=ax, color='red', markersize=3)
+census_gdf.plot(ax=ax, column='within_one_mile', cmap='coolwarm', legend=True, markersize=3)
+pharmacies_gdf.plot(ax=ax, color='black', markersize=3, marker='s')
 plt.show()
-
-
-
-
-
